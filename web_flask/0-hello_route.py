523376b594a60e58f8e6180374d7f8aeed75ab4c
William Mwendwa <50356076+mwendwa-will@users.noreply.github.com>
2023-10-23 10:04:45 +0300

    flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,16 @@
+
+#!/usr/bin/python3
+"""Flask framework
+    """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/airbnb-onepage/", strict_slashes=False)
+def hello_world():
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
